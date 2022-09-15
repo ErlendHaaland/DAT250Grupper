c9ba3f39966bd1287c8ef15bffdcb179241714b0
erlend <erlend@DESKTOP-HD417SL.localdomain>
2022-09-15 19:24:28 +0200

    Added SQL-injection mitigation

--- app/routes.py
+++ app/routes.py
@@ -13,8 +13,8 @@ def index():
     form = IndexForm()
 
     if form.login.is_submitted() and form.login.submit.data:
-        user = query_db('SELECT * FROM Users WHERE username="{}";'.format(form.login.username.data), one=True)
-        if user == None:
+        user = query_db("SELECT * FROM Users WHERE username = ?", [form.login.username.data], one=True)
+        if user is None:
             flash('Sorry, this user does not exist!')
         elif user['password'] == form.login.password.data:
             return redirect(url_for('stream', username=form.login.username.data))
@@ -22,8 +22,8 @@ def index():
             flash('Sorry, wrong password!')
 
     elif form.register.is_submitted() and form.register.submit.data:
-        query_db('INSERT INTO Users (username, first_name, last_name, password) VALUES("{}", "{}", "{}", "{}");'.format(form.register.username.data, form.register.first_name.data,
-         form.register.last_name.data, form.register.password.data))
+        query_db("INSERT INTO Users (username, first_name, last_name, password) VALUES(?, ?, ?, ?)", [form.register.username.data, form.register.first_name.data, form.register.last_name.data, form.register.password.data])
+        
         return redirect(url_for('index'))
     return render_template('index.html', title='Welcome', form=form)
 
@@ -32,17 +32,18 @@ def index():
 @app.route('/stream/<username>', methods=['GET', 'POST'])
 def stream(username):
     form = PostForm()
-    user = query_db('SELECT * FROM Users WHERE username="{}";'.format(username), one=True)
+    user = query_db("SELECT * FROM Users WHERE username=?", [username], one=True)
     if form.is_submitted():
         if form.image.data:
             path = os.path.join(app.config['UPLOAD_PATH'], form.image.data.filename)
             form.image.data.save(path)
 
 
-        query_db('INSERT INTO Posts (u_id, content, image, creation_time) VALUES({}, "{}", "{}", \'{}\');'.format(user['id'], form.content.data, form.image.data.filename, datetime.now()))
+        query_db("INSERT INTO Posts (u_id, content, image, creation_time) VALUES(?, ?, ?, ?)", [user['id'], form.content.data, form.image.data.filename, datetime.now()])
         return redirect(url_for('stream', username=username))
 
-    posts = query_db('SELECT p.*, u.*, (SELECT COUNT(*) FROM Comments WHERE p_id=p.id) AS cc FROM Posts AS p JOIN Users AS u ON u.id=p.u_id WHERE p.u_id IN (SELECT u_id FROM Friends WHERE f_id={0}) OR p.u_id IN (SELECT f_id FROM Friends WHERE u_id={0}) OR p.u_id={0} ORDER BY p.creation_time DESC;'.format(user['id']))
+    posts = query_db("SELECT p.*, u.*, (SELECT COUNT(*) FROM Comments WHERE p_id=p.id) AS cc FROM Posts AS p JOIN Users AS u ON u.id=p.u_id WHERE p.u_id IN (SELECT u_id FROM Friends WHERE f_id=?1) OR p.u_id IN (SELECT f_id FROM Friends WHERE u_id=?1) OR p.u_id=?1 ORDER BY p.creation_time DESC", [user['id']])
+    
     return render_template('stream.html', title='Stream', username=username, form=form, posts=posts)
 
 # comment page for a given post and user.
@@ -50,26 +51,26 @@ def stream(username):
 def comments(username, p_id):
     form = CommentsForm()
     if form.is_submitted():
-        user = query_db('SELECT * FROM Users WHERE username="{}";'.format(username), one=True)
-        query_db('INSERT INTO Comments (p_id, u_id, comment, creation_time) VALUES({}, {}, "{}", \'{}\');'.format(p_id, user['id'], form.comment.data, datetime.now()))
+        user = query_db("SELECT * FROM Users WHERE username= ?", [username], one=True)
+        query_db("INSERT INTO Comments (p_id, u_id, comment, creation_time) VALUES(?, ?, ?, ?)", [p_id, user['id'], form.comment.data, datetime.now()])
 
-    post = query_db('SELECT * FROM Posts WHERE id={};'.format(p_id), one=True)
-    all_comments = query_db('SELECT DISTINCT * FROM Comments AS c JOIN Users AS u ON c.u_id=u.id WHERE c.p_id={} ORDER BY c.creation_time DESC;'.format(p_id))
+    post = query_db("SELECT * FROM Posts WHERE id=?", [p_id], one=True)
+    all_comments = query_db("SELECT DISTINCT * FROM Comments AS c JOIN Users AS u ON c.u_id=u.id WHERE c.p_id=? ORDER BY c.creation_time DESC", [p_id])
     return render_template('comments.html', title='Comments', username=username, form=form, post=post, comments=all_comments)
 
 # page for seeing and adding friends
 @app.route('/friends/<username>', methods=['GET', 'POST'])
 def friends(username):
     form = FriendsForm()
-    user = query_db('SELECT * FROM Users WHERE username="{}";'.format(username), one=True)
+    user = query_db("SELECT * FROM Users WHERE username=?", [username], one=True)
     if form.is_submitted():
-        friend = query_db('SELECT * FROM Users WHERE username="{}";'.format(form.username.data), one=True)
+        friend = query_db("SELECT * FROM Users WHERE username=?", [form.username.data], one=True)
         if friend is None:
             flash('User does not exist')
         else:
-            query_db('INSERT INTO Friends (u_id, f_id) VALUES({}, {});'.format(user['id'], friend['id']))
+            query_db("INSERT INTO Friends (u_id, f_id) VALUES(?, ?)", [user['id'], friend['id']])
     
-    all_friends = query_db('SELECT * FROM Friends AS f JOIN Users as u ON f.f_id=u.id WHERE f.u_id={} AND f.f_id!={} ;'.format(user['id'], user['id']))
+    all_friends = query_db("SELECT * FROM Friends AS f JOIN Users as u ON f.f_id=u.id WHERE f.u_id=? AND f.f_id!=?", [user['id'], user['id']])
     return render_template('friends.html', title='Friends', username=username, friends=all_friends, form=form)
 
 # see and edit detailed profile information of a user
@@ -77,10 +78,8 @@ def friends(username):
 def profile(username):
     form = ProfileForm()
     if form.is_submitted():
-        query_db('UPDATE Users SET education="{}", employment="{}", music="{}", movie="{}", nationality="{}", birthday=\'{}\' WHERE username="{}" ;'.format(
-            form.education.data, form.employment.data, form.music.data, form.movie.data, form.nationality.data, form.birthday.data, username
-        ))
+        query_db("UPDATE Users SET education=?, employment=?, music=?, movie=?, nationality=?, birthday=? WHERE username=?", [form.education.data, form.employment.data, form.music.data, form.movie.data, form.nationality.data, form.birthday.data, username])
         return redirect(url_for('profile', username=username))
     
-    user = query_db('SELECT * FROM Users WHERE username="{}";'.format(username), one=True)
+    user = query_db("SELECT * FROM Users WHERE username=?", [username], one=True)
     return render_template('profile.html', title='profile', username=username, user=user, form=form)
