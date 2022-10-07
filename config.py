43084e944b48d51af690d8e02f9f04e43268fdbc
Erlend <Erlend.Haaland@hotmail.com>
2022-10-07 11:44:10 +0200

    Added randomly generated secret key

--- config.py
+++ config.py
@@ -3,7 +3,7 @@ import os
 # contains application-wide configuration, and is loaded in __init__.py
 
 class Config(object):
-    SECRET_KEY = os.environ.get('SECRET_KEY') or 'secret' # TODO: Use this with wtforms
+    SECRET_KEY = os.environ.get('SECRET_KEY') or 'F5Dfj1LN7fMelExqPqfmAe0MFodsy9QfhfzVrjIoaS0' # TODO: Use this with wtforms
     DATABASE = 'database.db'
     UPLOAD_PATH = 'app/static/uploads'
     ALLOWED_EXTENSIONS = {'jpeg', 'png', 'jpg'}
